bf385501d085e5987a3537aed111ea02070b8f62
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-06-11 17:41:46 -0500

    Update HojaVida.jsx

--- src/pages/HojaVida.jsx
+++ src/pages/HojaVida.jsx
@@ -8,8 +8,12 @@ import "../assets/css/inicio.css"
 import "../assets/css/tablas.css"
 import "../assets/css/modal.css"
 import UserProfile from "../components/UserProfile"
+import { usePerfil } from "../hooks/usePerfil";
+
+
 
 const HojaVida = () => {
+  const { perfil } = usePerfil(); 
   const { data: hojasVida, loading, error, refetch } = useHojasVida()
   const { data: equipos } = useEquipos()
   const { data: usuarios } = useUsuarios()
@@ -141,7 +145,7 @@ const HojaVida = () => {
     return <div>Error: {error}</div>
   }
 
-  return (
+ return (
     <div className="dashboard">
       <aside className="sidebar">
         <div className="sidebar-header">
@@ -149,7 +153,11 @@ const HojaVida = () => {
           <div className="theme-toggle">
             <i className="fa-solid fa-sun sun"></i>
             <label className="switch">
-              <input type="checkbox" checked={temaOscuro} onChange={toggleTheme} />
+              <input
+                type="checkbox"
+                checked={temaOscuro}
+                onChange={toggleTheme}
+              />
               <span className="slider"></span>
             </label>
             <i className="fa-solid fa-moon moon"></i>
@@ -170,7 +178,7 @@ const HojaVida = () => {
               </div>
             </div>
           </Link>
-
+          {(perfil?.Id_rol == "2" || perfil?.Id_rol == "1" || perfil?.Id_rol == "3") && (
           <Link to="/inventario" className="nav-item">
             <div className="nav-link-wrapper">
               <div className="item-glow inventory-glow"></div>
@@ -184,21 +192,23 @@ const HojaVida = () => {
               </div>
             </div>
           </Link>
-
-          <Link to="/mantenimiento" className="nav-item">
-            <div className="nav-link-wrapper">
-              <div className="item-glow maintenance-glow"></div>
-              <div className="nav-link-front">
-                <i className="fas fa-tools nav-icon maintenance-icon"></i>
-                <span>Mantenimiento</span>
+          )}
+          {(perfil?.Id_rol == "2" || perfil?.Id_rol == "1") && (
+            <Link to="/mantenimiento" className="nav-item">
+              <div className="nav-link-wrapper">
+                <div className="item-glow maintenance-glow"></div>
+                <div className="nav-link-front">
+                  <i className="fas fa-tools nav-icon maintenance-icon"></i>
+                  <span>Mantenimiento</span>
+                </div>
+                <div className="nav-link-back">
+                  <i className="fas fa-tools nav-icon maintenance-icon"></i>
+                  <span>Mantenimiento</span>
+                </div>
               </div>
-              <div className="nav-link-back">
-                <i className="fas fa-tools nav-icon maintenance-icon"></i>
-                <span>Mantenimiento</span>
-              </div>
-            </div>
-          </Link>
-
+            </Link>
+          )}
+          {(perfil?.Id_rol == "4" || perfil?.Id_rol == "1") && (
           <Link to="/Administracion" className="nav-item">
             <div className="nav-link-wrapper">
               <div className="item-glow maintenance-glow"></div>
@@ -212,7 +222,8 @@ const HojaVida = () => {
               </div>
             </div>
           </Link>
-
+         )}
+          {(perfil?.Id_rol == "2" || perfil?.Id_rol == "1" || perfil?.Id_rol == "3") && (
           <Link to="/prestamo" className="nav-item">
             <div className="nav-link-wrapper">
               <div className="item-glow lifecycle-glow"></div>
@@ -226,7 +237,8 @@ const HojaVida = () => {
               </div>
             </div>
           </Link>
-
+          )}
+          
           <Link to="/perfil" className="nav-item">
             <div className="nav-link-wrapper">
               <div className="item-glow profile-glow"></div>
@@ -256,6 +268,7 @@ const HojaVida = () => {
           </div>
         </nav>
       </aside>
+
 
       <main className="content">
         <div className="top-bar">
